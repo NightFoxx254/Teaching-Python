e3402fd6a04599e5be22fb0d33a5fc4f3ecb23bc
NightFoxx254 <153968273+NightFoxx254@users.noreply.github.com>
2024-08-09 07:15:44 -0700

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,55 @@
+#make a calculator that uses one input in the form of num,opperation,num
+
+
+
+#irradical = input("what operation are you using(as a symbol?")
+#won = int(input("what is the first number? "))
+#too = int(input("what is the second number? "))
+
+
+onek = input("what is your arithmancy problem?")
+won = int(onek[0])
+too = int(onek[1])
+
+
+
+
+
+
+if onek == "+":
+  print(won + too == treek)
+elif onek == "-":
+  print(won - too == treek)
+elif onek == "/":
+  print(won / too == treek)
+elif onek == "*":
+  print(won * too == treek)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#you can get the position of an input 
+#like this  input[1] and find out what the operation and numbers are with that
